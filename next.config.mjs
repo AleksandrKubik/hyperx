8073a0229ad98626ca4477d1431cc5e318932fdf
AKDesigne <webalekskubanov@gmail.com>
2025-02-26 07:26:51 +0100

    Fix: Optimize build size for Cloudflare Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
     },
-    webpack: (config) => {
-        config.resolve.fallback = { fs: false, path: false };
+    experimental: {
+        optimizePackageImports: ['@heroicons/react'],
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.optimization = {
+                ...config.optimization,
+                splitChunks: {
+                    chunks: 'all',
+                    minSize: 20000,
+                    maxSize: 24000000,
+                    cacheGroups: {
+                        default: false,
+                        vendors: false,
+                        commons: {
+                            name: 'commons',
+                            chunks: 'all',
+                            minChunks: 2,
+                        },
+                        lib: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name(module) {
+                                const packageName = module.context.match(
+                                    /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                                )[1];
+                                return `npm.${packageName.replace('@', '')}`;
+                            },
+                            chunks: 'all',
+                            minChunks: 1,
+                        },
+                    },
+                },
+            };
+        }
         return config;
     },
 }
